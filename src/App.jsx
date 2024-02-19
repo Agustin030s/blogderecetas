bb80097252541cc420742f3c62444ce0e8243725
lucianocabelloo <you@example.com>
2024-02-19 13:02:45 -0300

    Reseteo de css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
+import { Header } from './components/common/Header'
 
 function App() {
   return (
     <>
-      <h1>Hola mundo</h1>
+      <Header/>
     </>
   )
 }
---
+++ src/components/RecetasForm.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const RecetasForm = () => {
+  return (
+    <div>RecetasForm</div>
+  )
+}
