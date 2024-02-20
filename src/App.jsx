4abf59955abebb8912adf05cd5a7560c5cae97ff
lucianocabelloo <you@example.com>
2024-02-19 23:00:45 -0300

    Se creo el formulario de las recetas

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,23 @@ import Administrador from "./components/pages/Administrador";
 import Menu from "./components/common/Menu";
 import Footer from "./components/common/Footer";
 import Inicio from "./components/pages/Inicio";
+import RecetasForm from "./components/pages/recetas/RecetasForm";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Menu></Menu>
-      <Routes>
-        <Route exact path="/" element={<Inicio></Inicio>}></Route>
-        <Route
-          exact
-          path="/administrador"
-          element={<Administrador></Administrador>}
-        ></Route>
-      </Routes>
-      <Footer></Footer>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Menu></Menu>
+    //   <Routes>
+    //     <Route exact path="/" element={<Inicio></Inicio>}></Route>
+    //     <Route
+    //       exact
+    //       path="/administrador"
+    //       element={<Administrador></Administrador>}
+    //     ></Route>
+    //   </Routes>
+    //   <Footer></Footer>
+    // </BrowserRouter>
+    <RecetasForm/>
   );
 }
 
