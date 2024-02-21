5e96a4ab3afebd098d40900a782fac300e182fe5
mluchyb <122744366+mluchyb@users.noreply.github.com>
2024-02-21 11:40:28 -0300

    se agrego navbar

---
+++ src/components/common/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importa el archivo CSS de Bootstrap
+import { Navbar as BootstrapNavbar, Nav } from 'react-bootstrap'; // Importa los componentes de Navbar y Nav de react-bootstrap
+
+export const Navbar = () => {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="/Inicio">El Rincón Gourmet</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/Menu">Menu</Nav.Link>
+            <Nav.Link href="/Recetas">Recetas</Nav.Link>
+            <Nav.Link href="#/Administrador">Ingresa</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default Navbar;
+
