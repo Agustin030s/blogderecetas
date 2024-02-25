cd891d221e249f7c930a89e6f35a21a0db4b909c
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2024-02-25 17:19:24 -0300

    se agrego la ruta hacia el form para crear recetas en el componente App

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,20 @@ import Menu from "./components/common/Menu";
 import Footer from "./components/common/Footer";
 import Inicio from "./components/pages/Inicio";
 import Error404 from "./components/pages/Error404";
+import RecetasForm from "./components/pages/recetas/RecetasForm";
 
 function App() {
   return (
     <BrowserRouter>
       <Menu></Menu>
       <Routes>
-        <Route exact path="/" element={<Inicio></Inicio>}>
-        </Route>
-        <Route exact path="/administrador" element={<Administrador></Administrador>}>
-        </Route>
+        <Route exact path="/" element={<Inicio></Inicio>}></Route>
+        <Route
+          exact
+          path="/administrador"
+          element={<Administrador></Administrador>}
+        ></Route>
+        <Route exact path="/administrador/crear" element={<RecetasForm></RecetasForm>}></Route>
         <Route path="*" element={<Error404></Error404>}></Route>
       </Routes>
       <Footer></Footer>
