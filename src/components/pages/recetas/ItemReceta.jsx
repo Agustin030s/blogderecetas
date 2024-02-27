1f22bb82396789cec478ab9d9b2ca26759cfb7b8
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2024-02-27 12:43:12 -0300

    se creo la funcion borrarRecetaAPI en el archivo queries

--- src/components/pages/recetas/ItemReceta.jsx
+++ src/components/pages/recetas/ItemReceta.jsx
@@ -1,7 +1,51 @@
 import { Button, Image } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const ItemReceta = ({receta}) => {
+
+  const borrarReceta = () =>{
+    Swal.fire({
+      title: "¿Estás seguro de eliminar este producto?",
+      text: "No se puede revertir este paso",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Borrar",
+      cancelButtonText: "Cancelar",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        //agregar la logica que borre un producto mediante la api
+        const respuesta = await borrarProductoAPI(producto.id);
+        if (respuesta.status === 200) {
+          Swal.fire({
+            title: "Producto eliminado!",
+            text: `El producto "${producto.nombreProducto}" fue eliminado correctamente`,
+            icon: "success",
+          });
+          //actualizar tabla admin
+          const respuestaProductos = await leerProductoAPI();
+          if (respuestaProductos.status === 200) {
+            const productosRestantes = await respuestaProductos.json();
+            setProductos(productosRestantes);
+          } else {
+            Swal.fire({
+              title: "Ocurrio un error",
+              text: `Intenta está operación en unos minutos`,
+              icon: "error",
+            });
+          }
+        } else {
+          Swal.fire({
+            title: "Ocurrio un error",
+            text: `El producto ${producto.nombreProducto} no pudo ser eliminado, intente esta operación en unos minutos`,
+            icon: "error",
+          });
+        }
+      }
+    });
+  }
   
   return (
     <tr className="text-center">
@@ -19,7 +63,7 @@ const ItemReceta = ({receta}) => {
         <Link to={`/administrador/editar/` + receta.id} className="btn btn-warning me-2 mb-2">
           <i className="bi bi-pencil-square"></i>
         </Link>
-        <Button variant="danger" className="me-2 mb-2">
+        <Button variant="danger" className="me-2 mb-2" onClick={borrarReceta}>
           <i className="bi bi-trash3"></i>
         </Button>
       </td>
